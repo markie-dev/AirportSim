a0d245f8921814e670022d4c76aa05ea24886bad
marcus <marcus.d.ellison@gmail.com>
2021-10-03 19:37:32 -0500

    updated tiles.cpp

--- tiles.cpp
+++ tiles.cpp
@@ -14,13 +14,22 @@ void tiles::cntDec() {tilecnt--;}
 void tiles::loadTiles(string regionMap) {
     ifstream fin;
     int x, y, pollution, population;
-    string row1, row2, row3, row4, row5, row6, row7, row8, label;
-    tile *temp;
+    string row1, row2, row3, row4, row5, row6, row7, row8, label, word;
     pollution = 0;
     population = 0;
     x=0;
+    y=0;
 
     fin.open(regionMap);
+
+    getline(fin, row1);
+    stringstream s(row1);
+    while (getline(s, label, ',')) {
+        cout << word << endl;
+        tile newTile(label, x, y, pollution, population);
+        tileList.push_back(newTile);
+        x++;
+    }
 }
 
 void tiles::prtlist(){
@@ -28,7 +37,9 @@ void tiles::prtlist(){
 }
 
 void tiles::regionPrint(){
-
+    for (int i=0;i<tileList.size();i++){
+        cout << "Label: " << tileList[i].getLabel() << " Coordinates: " << tileList[i].getXCoord() << ", " << tileList[i].getYCoord() << endl;
+    }
 }
 
 void tiles::cleanup(){
